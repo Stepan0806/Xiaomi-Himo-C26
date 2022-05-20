6adf03e9f1a81dbc9e03763e4d93c73c668d8674
Stepan0806 <64284448+Stepan0806@users.noreply.github.com>
2022-05-20 10:02:17 +0300

    bike section

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+  });
